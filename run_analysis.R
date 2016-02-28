816918d56cfdae983f2d3b62598a410107af440a
gana100 <ganapathi100@gmail.com>
2016-02-28 19:37:27 +0530

    minor correction to code and addition of codebook

--- run_analysis.R
+++ run_analysis.R
@@ -5,18 +5,16 @@ download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 ##Get the list of the files from directory - UCI HAR Dataset (the unzip created this)
 path_rf <- file.path("./data" , "UCI HAR Dataset")
-dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
-dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-##Read subject files
-dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
-dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-## Read featpath_rf <- file.path("./data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
 ## Read relevent files
-## Read activity files
+## 1.Read activity files
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+## 2. Read subject files
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+## 3. Read features files
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
 #study the structure of the variables
